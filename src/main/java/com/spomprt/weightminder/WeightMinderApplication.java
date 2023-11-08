ca758bc463baf229cee3301f4e2ffacf0350f78f
khafizullinba <khafizullinba@pochtabank.ru>
2023-11-08 16:56:38 +0300

    initial commit

---
+++ src/main/java/com/spomprt/weightminder/WeightMinderApplication.java
@@ -0,0 +1,13 @@
+package com.spomprt.weightminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightMinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeightMinderApplication.class, args);
+    }
+
+}
