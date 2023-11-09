db30445c4722345aaf47ddfe621338980832052e
khafizullinba <khafizullinba@pochtabank.ru>
2023-11-09 16:34:01 +0300

    initial commit

--- src/main/java/com/spomprt/weightminder/helper/MessageHelper.java
+++ src/main/java/com/spomprt/weightminder/helper/MessageHelper.java
@@ -4,6 +4,10 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 
 public final class MessageHelper {
 
+    private MessageHelper() {
+        throw new UnsupportedOperationException();
+    }
+
     public static boolean isCommand(Message message) {
         return message.getText().startsWith("/");
     }
